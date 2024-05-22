bfb22e475231ff6f203dad2fd26f11872c34c6a4
ifatimahali <star18salfa@gmail.com>
2024-05-22 18:35:08 +0300

    f

---
+++ lab.js
@@ -0,0 +1,41 @@
+//q1
+
+for (let i = 25; i > 0; i--) {
+    if (i % 2 ==0){
+    console.log( "even" + i);
+}
+}
+
+// squere number
+for (let i =0; i <= 10; i++) {
+    console.log("squer number of " +i + "=" +i*i);
+}
+//odd number
+for (let i = 1; i < 20; i++) {
+    if (i % 2 != 0){
+    console.log( "odd" + i);
+}
+}
+//multiplication
+let x =1
+for (let i =1; i <= 10; i++) {
+    console.log(x + "X"+ i +"=" +i*x);
+}
+let y = 2
+for (let i =1; i <= 10; i++) {
+    console.log(y + "X"+ i +"=" +i*y);
+}
+let f = 3
+for (let i =1; i <= 9; i++) {
+    console.log(f + "X"+ i +"=" +i*f);
+}
+//last
+let s= 0 
+let a= 1
+let v=0
+let r= 0
+    console.log(s);
+    console.log(a);
+for (let i =0; i <= 10; i++) {
+    console.log(i);
+}
---
+++ main.js
@@ -0,0 +1,24 @@
+//add
+function addtwonumber(One , Tow){
+    return One + Tow
+}
+console.log (addtwonumber(18,3))
+
+//sub
+
+let num = function(one , tow){
+    return one - tow;
+}
+console.log (num(20,6))
+
+// multi
+let multi = function(oneNumber , towNumber){
+    return oneNumber * towNumber;
+}
+console.log (multi(2,6))
+
+//div 
+function divtwonumber(uum1 , uum2){
+    return uum1 / uum2
+}
+console.log (divtwonumber(18,3))
